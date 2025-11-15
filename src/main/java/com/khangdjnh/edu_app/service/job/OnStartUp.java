377be0d5aa7ba7f17d8f2fdee12d1b4e6ad6ac68
DinhKhang2002 <Khangdjnh24042002@gmail.com>
2025-11-15 11:22:02 +0700

    fix: config to server local

--- src/main/java/com/khangdjnh/edu_app/service/job/OnStartUp.java
+++ src/main/java/com/khangdjnh/edu_app/service/job/OnStartUp.java
@@ -2,10 +2,6 @@ package com.khangdjnh.edu_app.service.job;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.admin.client.Keycloak;
-import org.keycloak.representations.idm.RoleRepresentation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -14,44 +10,25 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Component
 @Slf4j
 public class OnStartUp {
 
-    @Autowired
-    private Keycloak keycloakAdmin;
-
-    @Value("${idp.realm}")
-    private String realm;
-
     @PostConstruct
     public void onStartup() throws IOException {
-        testLogin();
-//        initUploadDir();
+        log.info("Server's time: {}", Instant.now());
+        log.info("Local Time: {}", LocalDateTime.now());
+        initUploadDir();
     }
 
-
-
     private void initUploadDir() throws IOException {
-        log.info("Server's instant: {}", Instant.now());
-        log.info("Server's time: {}", LocalDateTime.now());
         String uploadDir = System.getenv("UPLOAD_DIR");
         if (uploadDir == null) uploadDir = "uploads/assignments/";
         Path path = Paths.get(uploadDir);
         if (!Files.exists(path)) {
             Files.createDirectories(path);
             log.info("Created upload directory at: {}", path.toAbsolutePath());
-        }
-    }
-
-    private void testLogin() {
-        try {
-            List<RoleRepresentation> roles = keycloakAdmin.realm(realm).roles().list();
-            System.out.println("Đăng nhập thành công! Số role trong realm: " + roles.size());
-        } catch (Exception e) {
-            System.err.println("Không thể đăng nhập Keycloak: " + e.getMessage());
         }
     }
 }
