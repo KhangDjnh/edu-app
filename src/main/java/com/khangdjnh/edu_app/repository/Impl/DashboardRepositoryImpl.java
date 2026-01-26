7afb5e75e572d8092a104668988940e88ebc4f1f
DinhKhang2002 <khang.dv204568@sis.hust.edu.vn>
2026-01-26 22:32:26 +0700

    fix: dashboard api

--- src/main/java/com/khangdjnh/edu_app/repository/Impl/DashboardRepositoryImpl.java
+++ src/main/java/com/khangdjnh/edu_app/repository/Impl/DashboardRepositoryImpl.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
-import java.sql.ResultSet;
 import java.util.List;
 
 @Repository
